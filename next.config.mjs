86ba33b56432136249c1ef744110631c288c6d2c
Moriyan1307 <aaryanmori@gmail.com>
2024-06-14 21:25:02 -0500

    Pwa enabled

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,26 @@
+// Import the Next.js and next-pwa modules using ESM syntax
+import next from "next";
+import withPWA from "next-pwa";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== "development",
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+// Define PWA configuration
+const pwaConfig = {
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+};
+
+// Wrap the Next.js configuration with PWA support
+export default withPWA(pwaConfig)(nextConfig);
