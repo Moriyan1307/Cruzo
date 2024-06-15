7d92a5093c97a1413d12119187804e3faa4d6ef4
Moriyan1307 <aaryanmori@gmail.com>
2024-06-14 23:02:36 -0500

    Added Dashboard

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,33 +4,36 @@ import { Box } from "@mui/material";
 import { hrPad, theme, vrPad } from "@/Utils/styleUtils";
 import Header from "../Components/Header/Header";
 import Head from "next/head";
+import Dashboard from "@/Components/Dashboard/Dashboard";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div>
-        <Head>
-          <title>Cruzo</title>
-          <meta
-            name="description"
-            content="Your Next.js application description"
-          />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-      </div>
-      <Box
-        sx={{
-          ...hrPad,
-          ...vrPad,
-          height: "100vh",
-          width: "100vw",
-          backgroundColor: theme.primary,
-        }}
-      >
-        <Header />
-      </Box>
+      <Dashboard />
     </>
   );
 }
+
+// <div>
+//         <Head>
+//           <title>Cruzo</title>
+//           <meta
+//             name="description"
+//             content="Your Next.js application description"
+//           />
+//           <link rel="icon" href="/favicon.ico" />
+//         </Head>
+//       </div>
+//       <Box
+//         sx={{
+//           ...hrPad,
+//           ...vrPad,
+//           height: "100vh",
+//           width: "100vw",
+//           backgroundColor: theme.primary,
+//         }}
+//       >
+//         <Header />
+//       </Box>
