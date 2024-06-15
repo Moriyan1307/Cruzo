17fe8541f2bc80fc56b3242a804c144745143f54
Moriyan1307 <aaryanmori@gmail.com>
2024-06-14 22:02:49 -0500

    Updated package

--- next.config.mjs
+++ next.config.mjs
@@ -3,16 +3,11 @@
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: "public", // Destination directory for the PWA files
-  //disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
-  register: true, // Register the PWA service worker
-  skipWaiting: true, // Skip waiting for service worker activation
+  dest: "public",
 });
 
 export default withPWA({
-  // Your Next.js config
   reactStrictMode: true,
-
   swcMinify: true,
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
